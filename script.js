47e8c18b7cf3ff7a8c6a0b735a199cd14372c565
Jad Kreit <84517030+Jad-Kreit29@users.noreply.github.com>
2021-06-11 17:46:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let dInput = 0
+let radius = 0
+let area = 0
+
+document.getElementById('right').onclick = movePageRight
+
+function movePageRight () {
+  document.body.style.backgroundColor = 'steelblue'
+  document.getElementById('left').style.display = 'block'
+  document.getElementById('right').style.display = 'none'
+  document.getElementById('page').innerHTML = 'Circumferance'
+}
+
+document.getElementById('left').onclick = movePageLeft
+
+function movePageLeft () {
+  document.body.style.backgroundColor = 'ghostwhite'
+  document.getElementById('left').style.display = 'none'
+  document.getElementById('right').style.display = 'block'
+  document.getElementById('page').innerHTML = 'Area'
+}
+
+document.getElementById('area-calculate-button').addEventListener('click', areaCalculate)
+
+function areaCalculate () {
+  dInput = document.getElementById('d-input').value
+  dInput = parseInt(dInput)
+  radius = dInput / 2
+  area = Math.PI * radius ** 2
+  alert(area)
+}
+
+function test () {
+  dInput = document.getElementById('d-input').value
+  dInput = parseInt(dInput)
+  radius = dInput / 2
+  document.getElementById('radius').style.display = 'block'
+  document.getElementById('radius-result').innerHTML = radius
+  if (document.getElementById('d-input').value == '') {
+    document.getElementById('radius').style.display = 'none'
+  }
+}
